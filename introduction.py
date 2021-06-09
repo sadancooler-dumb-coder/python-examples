a842a87bca32d55aef1248035be46dc1557d0893
sadancooler-dumb-coder <85643768+sadancooler-dumb-coder@users.noreply.github.com>
2021-06-09 23:02:24 +0400

    Create introduction.py

---
+++ introduction.py
@@ -0,0 +1,5 @@
+print("What is your name?")
+name = input('Name: ')
+print("What is your age?")
+age = input('Age: ')
+print("Your name is " + name + "\nYour age is " + age) # im sure something is wrong here
